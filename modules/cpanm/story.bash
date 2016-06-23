2c7454b5ab1c339a8aa683ed5d79f7ccd686bb19
Alexey Melezhik <melezhik@gmail.com>
2016-06-23 16:32:24 +0300

    bugfix

--- modules/cpanm/story.bash
+++ modules/cpanm/story.bash
@@ -9,7 +9,7 @@ if test -z $install_base; then
   if test -z $user; then
     cpanm $story_var $package  -q && echo install ok
   else
-    su --shell `which bash` --login -c "cpanm $story_var $package -q && echo install" $user
+    su --shell `which bash` --login -c "cpanm $story_var $package -q && echo install ok" $user
   fi
 
 else
@@ -19,7 +19,7 @@ else
   if test -z $user; then
     cpanm $story_var $package -q -l $install_base && echo install ok
   else
-    su --shell `which bash` --login -c "cpanm $story_var $package -q  -l $install_base && echo install" $user
+    su --shell `which bash` --login -c "cpanm $story_var $package -q  -l $install_base && echo install ok" $user
   fi
 
 fi
