819451a87336c60d916ffeca3119832c283afc58
Alexey Melezhik <melezhik@gmail.com>
2016-07-05 17:02:41 +0300

    bug fix

--- modules/cpanm/story.bash
+++ modules/cpanm/story.bash
@@ -1,8 +1,8 @@
 install_base=$(config install-base)
 user=$(config user)
 package=$(story_var package)
-http_proxy=$(config http_proxy)
-https_proxy=$(config https_proxy)
+export http_proxy=$(config http_proxy)
+export https_proxy=$(config https_proxy)
 
 if test "${http_proxy}"; then
   set_http_proxy="export http_proxy=${http_proxy};"
@@ -18,8 +18,6 @@ if test -z $install_base; then
   if test -z $user; then
 
     echo install $package ...
-
-    $set_https_proxy $set_http_proxy \
 
     cpanm $story_var $package  -q && echo install ok
 
@@ -37,7 +35,6 @@ else
 
   if test -z $user; then
     echo install $package into $install_base ...
-    $set_https_proxy $set_http_proxy \
     cpanm $story_var $package -q -l $install_base && echo install ok
   else
     echo install $package into $install_base user $user ...
